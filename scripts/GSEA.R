c28dcdcbcb8b6b3657286fc4e066a758a268a39a
Mongelard Fabien <fabien.mongelard@gmail.com>
2025-06-13 15:33:50 +0200

    2C

--- scripts/GSEA.R
+++ scripts/GSEA.R
@@ -4,48 +4,14 @@ library(here)
 library(readr)
 library(dplyr)
 
+# load microarray results
+ResultsTable_DE <- read_excel(here("data","ResultsTable_DE.xlsx"))
 
-Summ_Main_table <- read_csv("computed_data/Summ_Main_table.csv")
+load agilent chip decription 
+chip_list <- readr::read_delim(
+  "D:/smyl/Labo/Mouse/KO_NCL/Transcriptomics/Agilent/028005/028005_mm1_1747914873788/GeneList/028005_D_GeneList_20240520.txt",
+  delim = "\t")
 
-###########
-# ✅ 1. Overrepresentation Analysis (ORA)
-
-sig_genes <- Summ_Main_table$Gene_name[Summ_Main_table$p_value < 0.05 & abs(Summ_Main_table$log2FC) > log2(1.5)]
-write(sig_genes, file = here("computed_data", "sig_genes.txt"), sep= "\t" )
-
-###########
-# . Gene Set Enrichment Analysis (GSEA)
-
-library(clusterProfiler)
-library(org.Mm.eg.db)
-library(enrichplot)
-
-res_df <- Summ_Main_table %>%
-  dplyr::select(-c(Change, Significant, Color))
-
-res_df$score <- -log10(res_df$p_value) * sign(res_df$log2FC)
-gene_list <- res_df$score
-names(gene_list) <- res_df$Gene_name
-gene_list <- gene_list[!is.na(gene_list)]
-gene_list <- gene_list[!duplicated(names(gene_list))]
-gene_list <- sort(gene_list, decreasing = TRUE)
-
-gsea_res <- gseGO(geneList     = gene_list,
-                  OrgDb        = org.Mm.eg.db,
-                  keyType      = "SYMBOL",   # Or "ENTREZID" if needed
-                  ont          = "BP",       # Can be "MF" or "CC" too
-                  minGSSize    = 10,
-                  maxGSSize    = 500,
-                  pvalueCutoff = 0.05,
-                  verbose      = TRUE)
-
-# View top results
-head(gsea_res)
-
-# Plot enrichment of top term
-gseaplot2(gsea_res, geneSetID = gsea_res@result$ID[1])
-
-# Dotplot of top GO terms
-dotplot(gsea_res, showCategory = 20, split = ".sign") + facet_grid(.~.sign)
-
-write.csv(as.data.frame(gsea_res), "GSEA_GO_results.csv")
+list_expe <- select(ResultsTable_DE, ProbeName)
+list_ref <- select(chip_list, ProbeID) %>% rename(ProbeName = ProbeID)
+dplyr::setequal(list_expe,list_ref)
