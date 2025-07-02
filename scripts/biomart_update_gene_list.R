b51d3c5c2f110e7b317ec51ad57ac9a9c63a8452
Mongelard Fabien <fabien.mongelard@gmail.com>
2025-07-02 15:08:39 +0200

    lncrna and protein coding

--- scripts/biomart_update_gene_list.R
+++ scripts/biomart_update_gene_list.R
@@ -1,6 +1,6 @@
 # Try to update info about sequences on the microarray
 # using Ensemble fresh data
-# Hopefully will help with subsequent GSEA!
+# Hopefully will help with subsequent GSEA
 
 # Load biomaRt
 library(tidyverse)
@@ -12,11 +12,11 @@ library(rstudioapi)
 # Load probe data (columns: ProbeID, ExpressionValue)
 probe_data <- read_excel(here("data","ResultsTable_DE.xlsx"))
 
-# 3. Connect to Ensembl BioMart
+# 3. Connect to Ensembl BioMart or load available mart
 
 choice <- showQuestion(
   title = "Data Loading",
-  message = "Do you want to download data or load a local file?",
+  message = "Download mart from Ensembl OR load a local file?",
   ok = "Download",
   cancel = "Load Local"
 )
@@ -47,31 +47,59 @@ saveRDS(annotation, here("results","annotation_data.rds"))
 
 
 # 5. Merge with expression data
-updated_data <- left_join(probe_data, annotation, 
-                          by = c("ProbeName" = "agilent_sureprint_g3_ge_8x60k")) %>%  # CHANGE THIS
+updated_data <- left_join(probe_data,
+                          annotation,
+                          by = c("ProbeName" = "agilent_sureprint_g3_ge_8x60k")) %>%
   filter(
-    !is.na(external_gene_name),          # Remove NA values
-    external_gene_name != "",              # Remove empty strings
+    !is.na(external_gene_name),                # Remove NA values
+    external_gene_name != "",                  # Remove empty strings
     !str_detect(external_gene_name, "^[-/]"),  # Remove placeholders like "-" or "---"
-    Description != "Unknown", # Remove unknown description
-    transcript_biotype == "protein_coding"
+    Description != "Unknown" # Remove unknown description
+  )
+    
+lncRNA_GSEA_set <- updated_data %>%
+  filter(transcript_biotype == "lncRNA",
+    adj.P.Val_expe_controle <= 0.05
   )
 
-mouse_cols <- c(paste0("controle_", 1:4), paste0("expe_", 1:4))
+Protein_coding_GSEA_set <- updated_data %>%
+  filter(transcript_biotype == "protein_coding",
+         adj.P.Val_expe_controle <= 0.05
+  )
 
-tibble_for_GSEA <- updated_data %>%
+mouse_cols <- c(paste0("controle_", 1:4), paste0("expe_", 1:4), "adj.P.Val_expe_controle")
+
+lncRNA_GSEA_set <- lncRNA_GSEA_set %>%
   dplyr::select(
     "external_gene_name", "Description",
     "controle_1", "controle_2", "controle_3", "controle_4",
-    "expe_1", "expe_2", "expe_3", "expe_4" 
+    "expe_1", "expe_2", "expe_3", "expe_4" ,"adj.P.Val_expe_controle"
   ) %>%
   dplyr::rename(Name = external_gene_name) %>%
   group_by(Name) %>%
   summarise(
     Description = first(na.omit(Description)),
-    across(all_of(mouse_cols), ~ mean(.x, na.rm = TRUE)),
+    across(all_of(mouse_cols), ~ median(.x, na.rm = TRUE)),
     .groups = "drop"
-  )
+  )%>%
+  arrange(adj.P.Val_expe_controle) %>%
+  dplyr::select(-adj.P.Val_expe_controle)
+
+Protein_coding_GSEA_set <- Protein_coding_GSEA_set %>%
+  dplyr::select(
+    "external_gene_name", "Description",
+    "controle_1", "controle_2", "controle_3", "controle_4",
+    "expe_1", "expe_2", "expe_3", "expe_4" ,"adj.P.Val_expe_controle"
+  ) %>%
+  dplyr::rename(Name = external_gene_name) %>%
+  group_by(Name) %>%
+  summarise(
+    Description = first(na.omit(Description)),
+    across(all_of(mouse_cols), ~ median(.x, na.rm = TRUE)),
+    .groups = "drop"
+  )%>%
+  arrange(adj.P.Val_expe_controle) %>%
+  dplyr::select(-adj.P.Val_expe_controle)
 
 # 6. Save updated data
 write_tsv(tibble_for_GSEA, here("results", "updated_expression_data.txt"))
